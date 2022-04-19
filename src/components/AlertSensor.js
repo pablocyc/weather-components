ea533eb43cc00180a7bd26da3b1c002e8c753483
Pablo Cardozo <pablocyc83@gmail.com>
2022-04-19 13:09:14 -0400

    fix getTask

--- src/components/AlertSensor.js
+++ src/components/AlertSensor.js
@@ -4,6 +4,12 @@ class AlertSensor extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
+    this.minRange = "";
+    this.maxRange = "";
+    this.min = "";
+    this.max = "";
+    this.minOld = "";
+    this.maxOld = "";
   }
 
   static get styles() {
@@ -51,7 +57,8 @@ class AlertSensor extends HTMLElement {
         width: 24px;
       }
 
-      .input-max {
+      .input-max,
+      .input-min {
         background-color: transparent;
         border: 0;
         color: var(--text-color);
@@ -63,10 +70,13 @@ class AlertSensor extends HTMLElement {
         content: '✖';
         margin: 0 10px;
         color: lightcoral;
+        border: none;
+        box-shadow: none;
       }
       input:valid+span:after {
         content: '✓';
         margin: 0 5px;
+        padding: 4px;
       }
 
       .validity {
@@ -86,42 +96,68 @@ class AlertSensor extends HTMLElement {
 
   connectedCallback() {
     this.render();
+    this.init();
     const container = this.shadowRoot.querySelector(".container");
     container.addEventListener("click", e => this.sendData(e));
   }
 
+  init() {
+    getTask(this.sensor)
+      .then(docSnap => {
+        if (docSnap.exists()) {
+          const { min, max } = docSnap.data();
+          this.min = min;
+          this.max = max;
+          this.minOld = min;
+          this.maxOld = max;
+          const minInput = this.shadowRoot.querySelector(".input-min");
+          const maxInput = this.shadowRoot.querySelector(".input-max");
+          const minSpan = this.shadowRoot.querySelector(".valid-min");
+          const maxSpan = this.shadowRoot.querySelector(".valid-max");
+          minInput.value = min;
+          maxInput.value = max;
+          this.setStyleDisable(minInput, minSpan);
+          this.setStyleDisable(maxInput, maxSpan);
+        }
+      });
+  }
+
   setProps(sensor, min, max, step) {
     this.sensor = sensor;
-    this.min = min;
-    this.max = max;
+    this.minRange = min;
+    this.maxRange = max;
     this.step = step;
   }
 
   sendData(event) {
     const nodeName = event.target.nodeName;
     if (nodeName === "SPAN") {
-      const value = event.target.parentElement.querySelector("input").value;
+      const span = event.target.parentElement.querySelector("span");
+      const input = event.target.parentElement.querySelector("input");
+      const value = input.value;
       const key = event.path[1].classList[0];
-      updateTask(this.sensor, {
-        [key]: value
-      });
-      this.setStyleDisable(event);
-      const docSnap = getTask(this.sensor);
-      console.log(docSnap.data());
+      if (value !== this.minOld || value !== this.maxOld) {
+        this.minOld = value;
+        this.maxOld = value;
+        this.setStyleDisable(input, span);
+        updateTask(this.sensor, {
+          [key]: value
+        });
+      }
     }
     if (nodeName === "INPUT") {
       this.setStyleEnable(event);
     }
   }
 
-  setStyleDisable(event) {
-    event.target.parentElement.querySelector("input").style = "background-color: #0003; border: 1px solid #999999; border-radius: 5px;";
-    event.target.parentElement.querySelector("span").style = "color: var(--text-color);";
+  setStyleDisable(input, span) {
+    input.style = "background-color: #0003; border: 1px solid #999999; border-radius: 5px;";
+    span.style = "color: var(--text-color); border: none; box-shadow: none;";
   }
 
   setStyleEnable(event) {
     event.target.style = "background-color: inherit; border: inherit";
-    event.target.parentElement.querySelector("span").style = "color: green;";
+    event.target.parentElement.querySelector("span").style = "color: green; border: 1px solid #0005; box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06); border-radius: 5px;";
   }
 
   render() {
@@ -129,13 +165,13 @@ class AlertSensor extends HTMLElement {
     <style>${AlertSensor.styles}</style>
     <div class="container">
       <div class="min min-max">
-        <input type="number" class="input-max" placeholder="•" step="${this.step}" maxlength="2" max="${this.max}" min="${this.min}" required>
+        <input type="number" class="input-min" placeholder="•" step="${this.step}" maxlength="2" max="${this.maxRange}" min="${this.minRange}" required>
         <span class="validity valid-min"></span>
         <img src="icons/icon-min.svg" alt="icon min">
       </div>
       <img src="/icons/icon-${this.sensor}.svg" alt="icon sensor" class="icon">
       <div class="max min-max">
-        <input type="number" class="input-max" placeholder="•" step="${this.step}" maxlength="2" max="${this.max}" min="${this.min}" required>
+        <input type="number" class="input-max" placeholder="•" step="${this.step}" maxlength="2" max="${this.maxRange}" min="${this.minRange}" required>
         <span class="validity valid-max"></span>
         <img src="icons/icon-max.svg" alt="icon max">
       </div>
